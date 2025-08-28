e3d5a24bf16edd2371a142ff1fbe37d54c2e61a6
matthewDerous <matthew.derous@vdabcampus.be>
2025-08-28 10:42:22 +0200

    definier opbrengst

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class GoedeDoel {
 	private final String naam;
-	private BigDecimal opbrengst;
+	private BigDecimal opbrengst = BigDecimal.ZERO;
 
 	public GoedeDoel(String naam) {
 		this.naam = naam;
@@ -17,4 +17,6 @@ public class GoedeDoel {
 	public BigDecimal getOpbrengst() {
 		return opbrengst;
 	}
+
+
 }
